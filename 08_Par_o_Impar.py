e13029d87ecfe12f67a22491f14e4d93b88fd106
OACH-py <oach.core.sys@outlook.com>
2025-05-19 12:02:39 -0600

    Agregar implementación de la función para verificar si un número es par o impar

---
+++ 08_Par_o_Impar.py
@@ -0,0 +1,9 @@
+# solisitamos un número al usuario 
+n = int(input("Ingresa un número: "))
+
+# verificamos si el número es par o impar
+if n % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
+# fin del programa
